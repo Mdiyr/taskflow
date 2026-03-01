6eef4e41904670b25abe122bf864177f24e9615b
Mahdyar Moradi <knulp@Mahdyars-MacBook-Air.local>
2026-03-01 21:24:17 +0330

    [ADD] command switchs and functions

--- main.go
+++ main.go
@@ -7,15 +7,53 @@ import (
 	"os"
 )
 
+type User struct{
+	ID int
+	Email string
+	Password string
+}
+
+var userStorage []User
+
 func main() {
 	fmt.Println("Hello to TODO app")
 
 	command := flag.String("command", "no-command", "command to run")
 	flag.Parse()
 
-	scanner := bufio.NewScanner(os.Stdin)
+	for{
 
-	if *command == "create-task" {
+	runCommand(*command)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("please enter another command")
+	scanner.Scan()
+	*command = scanner.Text()
+
+	}
+
+	fmt.Printf("userStorage: %v\n", userStorage )
+}
+
+func runCommand(command string) {
+	switch command {
+	case "create-task":
+		createTask()
+	case "create-category":
+		createCategory()
+	case "register-user":
+		registerUser()
+	case "login":
+		login()
+	case "exit":
+		os.Exit(0)
+	default:
+		fmt.Println("command is not valid", command)
+	}
+
+} 
+func createTask() {
+		scanner := bufio.NewScanner(os.Stdin)
 		var name, duedate, category string
 
 		fmt.Println("please enter the task title")
@@ -31,6 +69,43 @@ func main() {
 		category = scanner.Text()
 
 		fmt.Println("task:", name, duedate, category)
-	}
-
 }
+func createCategory() {
+		scanner := bufio.NewScanner(os.Stdin)
+		var title, color string
+
+		fmt.Println("please enter the category title")
+		scanner.Scan()
+		title = scanner.Text()
+
+		fmt.Println("please enter the category color")
+		scanner.Scan()
+		color = scanner.Text()
+
+		fmt.Println("category:", title , color)
+}
+func registerUser() {
+		scanner := bufio.NewScanner(os.Stdin)
+		var id, email, password string
+
+		fmt.Println("please enter the user email")
+		scanner.Scan()
+		email = scanner.Text()
+
+		fmt.Println("please enter the user password")
+		scanner.Scan()
+		password = scanner.Text()
+
+		id = email
+
+		fmt.Println("user:", id, email, password)
+
+		user := User{
+			ID: len(userStorage) + 1,
+			Email: email,
+			Password: password,
+		}
+
+		userStorage = append(userStorage, user)
+}
+func login() {}
